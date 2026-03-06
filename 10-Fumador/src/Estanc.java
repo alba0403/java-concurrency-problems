204eb5adebad93059ff73d01dc5f96caeeb72ced
alba0403 <albamelendresnavarro@gmail.com>
2026-03-06 17:28:03 +0100

    10-fumadors commit final de classe

--- 10-Fumador/src/Estanc.java
+++ 10-Fumador/src/Estanc.java
@@ -6,6 +6,7 @@ public class Estanc {
   private List<Tabac> llistaTabac;
   private List<Paper> llistaPaper;
   private List<Llumi> llistaLlumi;
+  private boolean obert;
 
   // Constructor
   public Estanc() {
@@ -26,7 +27,47 @@ public class Estanc {
     }
   }
 
-  public void addTabac(){}
-  public void addLlumi(){}
-  public void addPaper(){}
+  public void addTabac(){
+    llistaTabac.add(new Tabac());
+    System.out.println("Afegint Tabac");
+    notifyAll();
+  }
+
+  public void addLlumi(){
+    llistaLlumi.add(new Llumi());
+    System.out.println("Afegint LlumÃ");
+    notifyAll();
+  }
+
+  public void addPaper(){
+    llistaPaper.add(new Paper());
+    System.out.println("Afegint Paper");
+    notifyAll();
+  }
+
+  private synchronized <Producte> Producte venProducte(List<Producte> llista) throws InterruptedException {
+        while (llista.isEmpty() && obert) {
+            wait();
+        }
+        if (!llista.isEmpty()) {
+            return llista.remove(0);
+        }
+        return null;
+    }
+
+    public Tabac venTabac() throws InterruptedException {
+        return venProducte(llistaTabac);
+    }
+    public Paper venPaper() throws InterruptedException {
+        return venProducte(llistaPaper);
+    }
+    public Llumi venLlumi() throws InterruptedException {
+        return venProducte(llistaLlumi);
+    }
+
+    public synchronized void tancarEstanc() {
+        obert = false;
+        System.out.println("Estanc tancat");
+        notifyAll();
+    }
 }
--- 10-Fumador/src/Fumador.java
+++ 10-Fumador/src/Fumador.java
@@ -1,3 +1,60 @@
-public class Fumador {
-  
+import java.util.Random;
+
+public class Fumador extends Thread {
+
+    private Estanc estanc;
+    private int id;
+    private Tabac tabac;
+    private Paper paper;
+    private Llumi llumi;
+    private int numFumades;
+    private Random random;
+
+    public Fumador(Estanc estanc, int id) {
+        this.estanc = estanc;
+        this.id = id;
+        this.random = new Random();
+    }
+
+    public void compraTabac() throws InterruptedException {
+        tabac = estanc.venTabac();
+        System.out.println("Fumador " + id + " comprant Tabac");
+    }
+
+    public void compraPaper() throws InterruptedException {
+        paper = estanc.venPaper();
+        System.out.println("Fumador " + id + " comprant Paper");
+    }
+
+    public void compraLlumi() throws InterruptedException {
+        llumi = estanc.venLlumi();
+        System.out.println("Fumador " + id + " comprant Llumí");
+    }
+
+    public void fuma() throws InterruptedException {
+        if (tabac != null && paper != null && llumi != null) {
+            System.out.println("Fumador " + id + " fumant");
+            tabac = null;
+            paper = null;
+            llumi = null;
+            numFumades++;
+            int espera = 500 + random.nextInt(501);
+            Thread.sleep(espera);
+            System.out.println("Fumador " + id + " ha fumat " + numFumades + " vegades");
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (numFumades < 3) {
+                compraTabac();
+                compraPaper();
+                compraLlumi();
+                fuma();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
