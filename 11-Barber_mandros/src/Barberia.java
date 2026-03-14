66f12942b9fd9e8566c1519297badf5c128a40f1
alba0403 <albamelendresnavarro@gmail.com>
2026-03-14 19:39:15 +0100

    11-barber creat metode main i logica entrar clients

--- 11-Barber_mandros/src/Barberia.java
+++ 11-Barber_mandros/src/Barberia.java
@@ -1,7 +1,7 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Barberia {
+public class Barberia extends Thread{
   // atributs
   private Queue<Client> salaEspera;
   private int numMaximCadires;
@@ -22,7 +22,31 @@ public class Barberia {
     return client;      //poll() asigna client a null si no hi han clients
   }
 
-  public void entrarClient(){
-    if
+  public void entrarClient(Client client){
+    synchronized(condBarber){
+      if(salaEspera.size() == numMaximCadires){
+        System.out.printf("No queden cadires, client %s se'n va", client.getNom());
+      } else {
+        salaEspera.add(client);
+        System.out.printf("Client %s en espera", client.getNom());
+        condBarber.notify();
+      }
+    }
+  }
+
+  public void run(){
+    for(int i = 0; i < 10; i++){
+
+    }
+
+  }
+
+  public static void main(String[] args) {
+    Barberia barberia = new Barberia(3);
+    Barber barber = new Barber("pepe");
+
+    //iniciem els fils
+    barberia.start();
+    barber.start();
   }
 }
